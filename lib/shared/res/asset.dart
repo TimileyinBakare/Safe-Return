f3e7b046900a787db45e3a2b9d516d307d5974fb
mc-stephen <stephenugo939@gmail.com>
2024-03-11 17:56:36 +0100

    new change for the safe lock

---
+++ lib/shared/res/asset.dart
@@ -0,0 +1,5 @@
+class AppAssets {
+  static const onboarding = "assets/svg/amico.svg";
+  static const onboarding1 = "assets/eclipseframe.png";
+  static const onboarding2 = "assets/eclipse8.png";
+}
---
+++ lib/shared/res/color.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const button_Color = 0xff17586E;
+}
